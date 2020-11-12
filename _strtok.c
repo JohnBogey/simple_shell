9c52d1b0eac0035148f22922f768de58e82d4fb8
mittsahl <msalem311@aol.com>
2020-11-12 19:11:34 +0000

    Working comnplete final version

--- _strtok.c
+++ _strtok.c
@@ -1,56 +1,51 @@
 #include "shell.h"
 
 /**
-* _strtok - takes a stirng snd splits it up into words
-* @str: string passed from main
-* Return: a pointer to array of strings or NULL
-*/
+ * _strtok - takes a stirng snd splits it up into words
+ * @str: string passed from main
+ * @c: delimiter of string
+ * Return: a pointer to array of strings or NULL
+ */
 
 char **_strtok(char *str, char c)
 {
-	int i, j, numOfWords = 0, charLength, wIdx = 0, cpyIdx, i2;
-	char **words;
+	int i, j, word_count = 0, word_length, word_index = 0, copy_index, temp;
+	char **word_list;
 
 	if (str == NULL || _strlen(str) == 0)
 		return (NULL);
 	for (i = 0; str[i]; i++)
-	{
 		if (str[i + 1] == c)
-			numOfWords++;
-	}
-	i--;
-	if (str[i] != c)
-		numOfWords++;
-	words = (char **)malloc(numOfWords * sizeof(char *) + 1);
-	if (words == NULL)
+			word_count++;
+	if (str[i - 1] != c)
+		word_count++;
+	word_list = (char **)malloc(word_count * sizeof(char *) + 1);
+	if (word_list == NULL)
 		return (NULL);
 	for (i = 0; str[i]; i++)
-	{
 		if ((str[i] == c || i == 0) && str[i + 1] != c && str[i + 1] != '\0')
 		{
 			if (str[i] == c)
 				i++;
-			charLength = 1;
-			i2 = i;
-			cpyIdx = 0;
+			word_length = 1;
+			temp = i;
+			copy_index = 0;
 			for (j = i; str[j] != c && str[j]; j++)
-				charLength++;
-			words[wIdx] = (char *)malloc(charLength);
-			if (words[wIdx] == NULL)
+				word_length++;
+			word_list[word_index] = (char *)malloc(word_length);
+			if (word_list[word_index] == NULL)
 			{
-				for (; wIdx > 0; wIdx--)
-					free(words[wIdx]);
-				free(words);
+				for (; word_index > 0; word_index--)
+					free(word_list[word_index]);
+				free(word_list);
 				return (NULL);
 			}
-			words[wIdx][charLength] = '\0';
-			for (; i2 < j; i2++, cpyIdx++)
-				words[wIdx][cpyIdx] = str[i2];
-			wIdx++;
+			word_list[word_index][word_length] = '\0';
+			for (; temp < j; temp++, copy_index++)
+				word_list[word_index][copy_index] = str[temp];
+			word_index++;
 		}
-	}
-	wIdx++;
-	words[wIdx] = '\0';
-	return (words);
+	word_index++;
+	word_list[word_index] = '\0';
+	return (word_list);
 }
-
