11ec665aeefc369dc7437625e966c12dc70e1ba8
JohnBogey <maxwellloganlovell@gmail.com>
2020-11-12 20:44:35 +0000

    11_12_20 file_add trying out signals

---
+++ signal_test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include  <signal.h>
+
+void c_blocker(int sig)
+{
+	signal(sig, SIG_IGN);
+	printf(" Pathetic.\nYou can't stop me.\n");
+	signal(SIGINT, c_blocker);
+}
+
+void main (void)
+{
+	int i = 10;
+
+	signal(SIGINT, c_blocker);
+	while (i >= 0)
+	{
+		printf("%i\n", i--);
+		sleep(1);
+	}
+}
