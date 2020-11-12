a79d6e721f647eb43d804856e8477a644289eab2
mittsahl <msalem311@aol.com>
2020-11-12 16:25:20 +0000

    Functions for freeing malloc'd lines &2d arrays

---
+++ mem.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+void free1d(char *line)
+{
+	free(line);
+	return;
+}
+
+void free2d(char **lines)
+{
+	char **current = lines;
+	char *line;
+	int i = 0;
+	while (*current)
+	{
+		i++;
+		current++;
+	}
+	for (; i >= 0; i--)
+	{
+		line = lines[i];
+		free(line);
+	}	
+	free(lines);
+	return;
+}
