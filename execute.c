a1f08dbd2355572e4f4458d059370c4b411627ab
JohnBogey <maxwellloganlovell@gmail.com>
2020-11-12 19:33:06 +0000

    11_12_20 added framework for built ins

--- execute.c
+++ execute.c
@@ -5,10 +5,41 @@
 #include <sys/wait.h>
 
 /**
- * exec_built - executes built in function
+ * struct builtin - Struct builtin
  *
- *
+ * @cmd: command being checked
+ * @f: The function associated
  */
+typedef struct builtin
+{
+	char *cmd;
+	char *(*func)(char **argv);
+} built_t;
+
+/**
+ * exec_builtin - matches input to function and runs function
+ * @argv: null terminated string of args for program
+ * Return: 0 on success, -1 on failure
+ */
+int exec_builtin(char **argv)
+{
+	built_t function[] = {
+		{"exit", func_exit},
+		{"cd", func_cd},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	for (i = 0; *(function + i)->cmd, i++)
+		if (*(function + i)->cmd == argv[0])
+		{
+			function = *(function + i)->func;
+			function(argv);
+			return (0);
+		}
+	return (-1);
+}
 
 /**
  * exec_prog - uses children to execve program
