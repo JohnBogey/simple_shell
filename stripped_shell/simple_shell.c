df080c612ee4350b96c6aad4e84f2845ded7da8a
JohnBogey <maxwellloganlovell@gmail.com>
2020-11-17 20:41:32 +0000

    file_add stripped shell

---
+++ stripped_shell/simple_shell.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+#include <string.h>
+#include <stdbool.h>
+#include <signal.h>
+
+/**
+ * kill_block - stops ctrl+c
+ * Return: always nothing
+ */
+void kill_block(int sig)
+{
+	(void)signal;
+	_puts("\n$ ");
+}
+
+/**
+ * main - a very simple shell
+ * Return: always 0
+ */
+int main(void)
+{
+	char *line = NULL;
+	size_t size = 0;
+	char **cmd;
+	int status = 1;
+
+	signal(SIGINT, kill_block);
+	do {
+		if (isatty(STDIN_FILENO) != 0)
+			_puts("$ ");
+		line = NULL;
+		size = 0;
+		if (getline(&line, &size, stdin) == -1)
+		{
+			_putchar('\n');
+			free(line);
+			status = 0;
+			continue;
+		}
+		if (line[0] != '\n')
+			printf("not found\n");
+		free(line);
+	} while (status);
+	return (0);
+}
