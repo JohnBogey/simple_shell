94760e2e149824d6b95b63df37558c369598970a
JohnBogey <maxwellloganlovell@gmail.com>
2020-11-19 19:58:58 +0000

    newline support

--- simple_shell.c
+++ simple_shell.c
@@ -43,16 +43,15 @@ int main(void)
 		}
 		if (line[0] != '\n')
 		{
+			/*set last newline to nullbyte*/
+			line[_strlen(cmd[0]) - 1] = '\0';
 			/*set cmd to array of commands/flags*/
 			cmd = _strtok(line, ' ');
-			/*set new line char to null byte*/
-
-			cmd[0][_strlen(cmd[0]) - 1] = '\0';
 			/*check built ins, run if found*/
 			status = exec_builtin(cmd);
 			if (status == -1)
 			{
-				/*turns cmc to args for exec*/
+				/*turns cmd to args for exec*/
 
 				/*execute program if found*/
 				exec_prog(cmd);
