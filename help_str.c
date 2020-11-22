3a5bc7171a6d3070f627ad6b610cd202d699c08b
JohnBogey <maxwellloganlovell@gmail.com>
2020-11-22 20:14:45 +0000

    fixed most betty errors

--- help_str.c
+++ help_str.c
@@ -1,55 +1,5 @@
 #include "shell.h"
 
-
-/**
- * _puts - prints a line to stdout
- * Return: void
- * @str: string passed to function from main
- */
-
-void _putsNewLine(char *str)
-{
-        char *print = str;
-
-        while (*print)
-        {
-                _putchar(*print);
-                print++;
-        }
-	_putchar('\n');
-}
-
-
-
-/**
- * _puts - prints a line to stdout
- * Return: void
- * @str: string passed to function from main
- */
-
-void _puts(char *str)
-{
-	char *print = str;
-
-	while (*print)
-	{
-		_putchar(*print);
-		print++;
-	}
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 /**
  * _strcpy - copys a string
  * Return: pointer to copy
--- mem.c
+++ mem.c
@@ -1,14 +1,18 @@
 #include "shell.h"
 
+/**
+ * free2d - frees 2d array
+ * @lines: 2d array
+ * Return: nothing
+ */
 void free2d(char **lines)
 {
 	int i = 0;
 
-	while(lines[i] != NULL)
+	while (lines[i] != NULL)
 	{
 		free(lines[i]);
 		i++;
 	}
 	free(lines);
-	return;
 }
